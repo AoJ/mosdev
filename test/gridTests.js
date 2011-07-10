524c65c1e72d53348ecfe3f004aff34ce0b37029
Roy Jacobs <roy.jacobs@gmail.com>
2011-07-10 16:04:31 +0200

    Wrapped head around Jasmine tests

--- test/gridTests.js
+++ test/gridTests.js
@@ -1,14 +1,42 @@
 describe("Grid", function() {
-    var grid = new mos.grid();
+    beforeEach(function() {
+        this.grid = new mos.grid();
+    });
 
     it("can be constructed with default options", function() {
-        expect(grid).not.toBe(undefined);
+        expect(this.grid).not.toBe(undefined);
     });
 
     // This spec shows up as being "skipped", even though "can be added" is run.
     describe("Rows", function() {
+        beforeEach(function() {
+            this.grid.rows.push({ a: "a" });
+        });
+
         it("can be added", function() {
-            grid.rows.push({ a: "a" });
+            expect(this.grid.rows()[0]).toEqual({ a: "a" });
+        });
+    });
+
+    describe("DisplayRows", function() {
+        beforeEach(function() {
+            this.grid.rows.push({ a: "a", b: "b" });
+            this.grid.columns.push(new mos.grid.column({
+                property: "b"
+            }));
+        });
+
+        it("match the number of rows", function() {
+            expect(this.grid.displayRows().length).toBe(1);
+        });
+
+        // There is no "toBeInstanceOf" yet
+        xit("contain cells", function() {
+            expect(this.grid.displayRows()[0][0]).toBeInstanceOf("cell");
+        });
+
+        it("contain the correct property of a row", function() {
+            expect(this.grid.displayRows()[0][0].displayValue).toBe("b");
         });
     });
 });
